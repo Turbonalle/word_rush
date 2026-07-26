7a294ba7f37075f06ade1ccb068919962da2d415
Turbonalle <jonathanbagger@hotmail.com>
2026-07-26 22:35:48 +0300

    Added language option

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,17 @@ const playButton = document.getElementById("play-button");
 const backButton = document.getElementById("back-button");
 const lengthSlider = document.getElementById("length-slider");
 const lengthValue = document.getElementById("length-value");
+const englishButton = document.getElementById("option-en");
+const swedishButton = document.getElementById("option-sv");
+
+const gameState = {
+	screen: "menu",
+	language: "en",
+	letters: 6,
+	word: "",
+	availableWords: [],
+	wordsFound: []
+};
 
 function showGame() {
 	menuScreen.classList.add("hidden");
@@ -14,6 +25,18 @@ function showMenu() {
 	menuScreen.classList.remove("hidden");
 	gameScreen.classList.add("hidden");
 }
+
+englishButton.addEventListener("click", () => {
+	swedishButton.classList.remove("active");
+	englishButton.classList.add("active");
+	gameState.language = "en";
+});
+
+swedishButton.addEventListener("click", () => {
+	englishButton.classList.remove("active");
+	swedishButton.classList.add("active");
+	gameState.language = "sv";
+});
 
 playButton.addEventListener("click", () => {
 	// startGame(options);
