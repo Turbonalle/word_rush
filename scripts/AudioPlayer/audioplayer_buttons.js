9fa4a4c011484ed745d7b7b5c9c46b11cc0dc1b3
Turbonalle <jonathanbagger@hotmail.com>
2026-08-08 12:50:57 +0300

    AudioPlayer's playlist toggle works.

--- scripts/AudioPlayer/audioplayer_buttons.js
+++ scripts/AudioPlayer/audioplayer_buttons.js
@@ -3,15 +3,6 @@ import { AudioPlayer } from "./AudioPlayer.js";
 const playListcontainer = document.getElementById("playlist-container");
 const toggleButton = document.getElementById("toggle-playlist-button");
 
-document.getElementById("previous-button").addEventListener("click", () => { AudioPlayer.previous(); })
-document.getElementById("play-button").addEventListener("click", () => { AudioPlayer.togglePlay(); })
-document.getElementById("stop-button").addEventListener("click", () => { AudioPlayer.stop(); })
-document.getElementById("next-button").addEventListener("click", () => { AudioPlayer.next(); })
-toggleButton.addEventListener("click", () => {
-	playListcontainer.classList.toggle("hidden");
-	toggleButton.textContent = playListcontainer.classList.contains("hidden") ? "▼" : "▲" ;
-});
-
 function buildPlaylist() {
 	const container = document.getElementById("playlist-container");
 	container.replaceChildren();
@@ -26,4 +17,16 @@ function buildPlaylist() {
 	});
 }
 
-buildPlaylist();
+document.getElementById("previous-button").addEventListener("click", () => { AudioPlayer.previous(); })
+document.getElementById("play-button").addEventListener("click", () => { AudioPlayer.togglePlay(); })
+document.getElementById("stop-button").addEventListener("click", () => { AudioPlayer.stop(); })
+document.getElementById("next-button").addEventListener("click", () => { AudioPlayer.next(); })
+toggleButton.addEventListener("click", () => {
+	playListcontainer.classList.toggle("hidden");
+	toggleButton.textContent = playListcontainer.classList.contains("hidden") ? "▼" : "▲" ;
+	if (playListcontainer.classList.contains("hidden")) {
+		playListcontainer.replaceChildren();
+	} else {
+		buildPlaylist();
+	}
+});
