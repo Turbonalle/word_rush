7d0e9f7bdb7ad881ce2a4976825f19efd65b15a5
Turbonalle <jonathanbagger@hotmail.com>
2026-08-26 22:05:37 +0300

    Created a basic progress-screen design.

--- scripts/progress_screen.js
+++ scripts/progress_screen.js
@@ -1,12 +1,10 @@
 import { modeToProgress, progressToMode } from "./screen_switch.js";
 
 document.getElementById("mode-to-progress-button").addEventListener("click", () => {
-	console.log("Hello!!!");
 	modeToProgress();
 });
 
 document.getElementById("progress-to-mode-button").addEventListener("click", () => {
-	console.log("Hello back!!!");
 	progressToMode();
 });
 
