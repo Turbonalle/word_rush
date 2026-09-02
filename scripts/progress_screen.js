f26e694c056cea7b1dce66822b523c61928e3d79
Turbonalle <jonathanbagger@hotmail.com>
2026-09-02 21:56:45 +0300

    Changed unlockables visual appearance.

--- scripts/progress_screen.js
+++ scripts/progress_screen.js
@@ -2,6 +2,10 @@ import { Save } from "./save.js";
 import { ProgressManager } from "./ProgressManager.js";
 import { modeToProgress, progressToMode } from "./screen_switch.js";
 
+let SONG_SVG = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m9 9 10.5-3m0 6.553v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 1 1-.99-3.467l2.31-.66a2.25 2.25 0 0 0 1.632-2.163Zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 0 1-.99-3.467l2.31-.66A2.25 2.25 0 0 0 9 15.553Z" />
+</svg>`;
+
 function updateProgressScreen() {
 	// Games
 	document.getElementById("stat-gamesplayed").textContent = Save.getGamesPlayed();
@@ -26,6 +30,12 @@ function updateProgressScreen() {
 	//Unlockables
 	document.getElementById("finishedDailyLevel").classList.toggle("unlocked", ProgressManager.isAchievementUnlocked("finishedDailyLevel"));
 	document.getElementById("finishedZenLevel").classList.toggle("unlocked", ProgressManager.isAchievementUnlocked("finishedZenLevel"));
+	document.getElementById("finishedHardLevel").classList.toggle("unlocked", ProgressManager.isAchievementUnlocked("finishedHardLevel"));
+	document.getElementById("finishedPanicLevel").classList.toggle("unlocked", ProgressManager.isAchievementUnlocked("finishedPanicLevel"));
+
+	document.querySelectorAll(".song-svg").forEach(element => {
+		element.innerHTML = SONG_SVG;
+	});
 }
 
 document.getElementById("mode-to-progress-button").addEventListener("click", () => {
