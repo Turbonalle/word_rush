7ae54c3e491412f917023b1b38ff08e2a5add66e
Turbonalle <jonathanbagger@hotmail.com>
2026-09-06 08:32:46 +0300

    Added winning theme.

--- scripts/AudioPlayer/audioplayer_buttons.js
+++ scripts/AudioPlayer/audioplayer_buttons.js
@@ -1,11 +1,9 @@
 import { AudioPlayer } from "./AudioPlayer.js";
 
-const toggleButton = document.getElementById("toggle-playlist-button");
-
 document.getElementById("previous-button").addEventListener("click", () => { AudioPlayer.previous(); })
 document.getElementById("play-button").addEventListener("click", () => { AudioPlayer.togglePlay(); })
 document.getElementById("stop-button").addEventListener("click", () => { AudioPlayer.stop(); })
 document.getElementById("next-button").addEventListener("click", () => { AudioPlayer.next(); })
-toggleButton.addEventListener("click", () => {
+document.getElementById("toggle-playlist-button").addEventListener("click", () => {
 	AudioPlayer.togglePlaylist();
 });
--- scripts/AudioPlayer/playlists.js
+++ scripts/AudioPlayer/playlists.js
@@ -3,5 +3,10 @@ export const menuPlaylist = [
 		id: "song1",
 		title: "Wordrush Menu",
 		path: "audio/wordrush_menu.mp3"
+	},
+	{
+		id: "song2",
+		title: "Winning Theme",
+		path: "audio/wordrush_winning_theme.mp3"
 	}
 ];
